10f973f07e220da4b833a39f671fb8c76ad1f23d
Martin <martinfz@t-online.de>
2022-12-30 15:22:33 +0100

    Feature: All top250 movies and printing current movie

--- movie_owner_visualization/get_top250_movie_list.py
+++ movie_owner_visualization/get_top250_movie_list.py
@@ -53,10 +53,11 @@ def flattenMovieDict(movie_dict):
 
 
 movie_list = []
-for movie in top250[:3]:
+for movie in top250:
+    print(movie["title"])
     currentMovie = ia.get_movie(movie.movieID)
     dict_movie = dict(currentMovie)
     ready_dict_movie = flattenMovieDict(dict_movie)
     movie_list.append(ready_dict_movie)
 
-exportObjectToJSON(movie_list, "export_movies")
+exportObjectToJSON(movie_list, "top250_movies")
