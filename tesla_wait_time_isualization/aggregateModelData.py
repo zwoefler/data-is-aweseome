c78e9074ffd4a151b1a119373254c21a4b800cac
Martin <martinfz@t-online.de>
2023-02-06 23:02:50 +0100

    Feature: Aggregating all Model3 data int one file with price and date

--- tesla_wait_time_isualization/aggregateModelData.py
+++ tesla_wait_time_isualization/aggregateModelData.py
@@ -6,37 +6,47 @@ import os
 
 # Write function to get ALL data for a given Model
 
-trimID = "$MT324"
 data_dir = "final_data"
 locale = "en_US"
 model = "m3"
 files = f"{model}_{locale}_*json"
 
-exportFileName = f"data_{model}_{trimID}_{locale}.json"
+exportFileName = f"data_{model}_{locale}.json"
 
 def exportModelData(jsonData):
     with open(exportFileName, "w") as f:
         json.dump(jsonData, f)
 
-
+####################################################
 
 json_files = glob.glob(os.path.join(data_dir, files))
 
 priceData = []
 
+exportData= {
+    model: {}
+}
+
 for file_path in json_files:
     with open(file_path, "r") as f:
         data = json.load(f)
 
+    print("FILE:", file_path)
     trims = data["trims"]
 
-    for trim in trims:
-        if trim["trimShorthandle"] == trimID:
-            price = trim["price"]
-            date = data["date"]
-            priceData.append((date, price))
+    for trimObj in trims:
+        if (trimObj["trimShorthandle"] not in exportData[model]):
+            exportData[model][trimObj["trimShorthandle"]] = {
+                "data": [],
+                "name": trimObj["displayedName"]
+            }
+        price = trimObj["price"]
+        date = data["date"]
+        exportData[model][trimObj["trimShorthandle"]]["data"].append((date, price))
 
-sorted_data = sorted(priceData, key=lambda x: x[0])
-dates, prices = zip(*sorted_data)
 
-exportModelData(sorted_data)
+for model in exportData.keys():
+    for trim, trimData in exportData[model].items():
+        trimData["data"] = sorted(trimData["data"], key=lambda x: x[0])
+
+exportModelData(exportData)
