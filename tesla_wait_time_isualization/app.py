e5145514c3b9535d6af49fc2c6db9ffff1fa8cdc
Martin <martinfz@t-online.de>
2023-02-07 00:24:17 +0100

    Feautre: Simple graph of price movement for Model Data!

--- tesla_wait_time_isualization/app.py
+++ tesla_wait_time_isualization/app.py
@@ -1,24 +1,39 @@
 from flask import Flask, render_template, request
+import matplotlib.pyplot as plt
+import io
+import base64
 import json
 
 app = Flask(__name__)
 
-# @app.route("/")
-# def index():
-#     with open("aggregatedData_m3_en_US.json", "r") as f:
-#         model_data = json.load(f)
-#     models = list(model_data.keys())
-#     return render_template("index.html", models=models, modelData=model_data)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     with open('aggregatedData_m3_en_US.json', 'r') as f:
-        data = json.load(f)
+        model_data = json.load(f)
 
-    trims = []
-    trims = list(data["m3"].keys())
+    if request.method == 'POST':
+        model = "m3"
+        trim = request.form['trim']
+        data = model_data[model][trim]['data']
 
-    return render_template('index.html', data=data, trims=trims)
+        x = [item[0] for item in data]
+        y = [item[1] for item in data]
+
+        fig = plt.figure()
+        plt.plot(x, y)
+        plt.xlabel('Timestamp')
+        plt.ylabel('Price')
+        plt.title(f"{model} {trim} Price over time")
+
+        image = io.BytesIO()
+        fig.savefig(image, format='png')
+        image.seek(0)
+        image_base64 = base64.b64encode(image.getvalue()).decode('utf-8')
+
+        return render_template('index.html', model_data=model_data, trim=trim, image=image_base64)
+
+
+    return render_template('index.html', model_data=model_data)
 
 
 if __name__ == "__main__":
