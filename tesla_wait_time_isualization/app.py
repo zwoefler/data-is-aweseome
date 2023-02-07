6fd324b66500e0f20b0348b16546292098d4c4b2
Martin <martinfz@t-online.de>
2023-02-07 12:31:54 +0100

    Feature: Select all models and trims!

--- tesla_wait_time_isualization/app.py
+++ tesla_wait_time_isualization/app.py
@@ -4,39 +4,52 @@ import io
 import base64
 import json
 from datetime import datetime
+import logging
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+with open('aggregatedData_en_US_model_data.json', 'r') as f:
+    model_data = json.load(f)
+
+models = model_data.keys()
+trims = []
+
+@app.route("/model_selected", methods=["POST"])
+def model_selected():
+    model = request.form["model"]
+    logging.warning(model)
+    trims = list(model_data[model].keys())
+    return render_template("index.html", models=models, selected_model=model, trims=trims)
+
+
+@app.route("/trim_selected", methods=["POST"])
+def trim_selected():
+    trim = request.form['trim']
+    model = request.form['model']
+
+    data = model_data[model][trim]['data']
+
+    dates = [item[0] for item in data]
+    prices = [item[1] for item in data]
+    dates = [datetime.fromtimestamp(date / 1000) for date in dates]
+
+    fig = plt.figure()
+    plt.plot(dates, prices)
+    plt.xlabel('Timestamp')
+    plt.ylabel('Price')
+    plt.title(f"{model} {trim} Price over time")
+
+    image = io.BytesIO()
+    fig.savefig(image, format='png')
+    image.seek(0)
+    image_base64 = base64.b64encode(image.getvalue()).decode('utf-8')
+
+    return render_template('index.html', models=models, trims=trims, selected_model=model, selected_trim=trim, image=image_base64)
+
+
+@app.route('/', methods=['GET'])
 def index():
-    with open('aggregatedData_m3_en_US.json', 'r') as f:
-        model_data = json.load(f)
-
-    if request.method == 'POST':
-        model = "m3"
-        trim = request.form['trim']
-        data = model_data[model][trim]['data']
-
-        dates = [item[0] for item in data]
-        prices = [item[1] for item in data]
-        dates = [datetime.fromtimestamp(date / 1000) for date in dates]
-
-
-        fig = plt.figure()
-        plt.plot(dates, prices)
-        plt.xlabel('Timestamp')
-        plt.ylabel('Price')
-        plt.title(f"{model} {trim} Price over time")
-
-        image = io.BytesIO()
-        fig.savefig(image, format='png')
-        image.seek(0)
-        image_base64 = base64.b64encode(image.getvalue()).decode('utf-8')
-
-        return render_template('index.html', model_data=model_data, trim=trim, image=image_base64)
-
-
-    return render_template('index.html', model_data=model_data)
+    return render_template('index.html', models=models)
 
 
 if __name__ == "__main__":
