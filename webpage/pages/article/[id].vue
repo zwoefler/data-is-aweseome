e7a8fab3959cca6737ab5dc33b8c1244ccdbe2eb
Martin <martinfz@t-online.de>
2023-02-13 11:11:58 +0100

    Feature: Article with correct image to article from Strapi

--- webpage/pages/article/[id].vue
+++ webpage/pages/article/[id].vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    {{ response }}
+    <p id="title" class="font-bold">{{ article.title }}</p>
+    <p id="published">{{ article.publishedAt }}</p>
+    <br>
+    <img :src="imageURL" alt="">
+    <div>
+      <p>{{ article.text }}</p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
   import type { Articles } from '~/types'
-  const { find } = useStrapi()
-  const response = await find<Article>('articles')
+
+  const { findOne } = useStrapi()
+  const route = useRoute()
+  const baseURL = useStrapiUrl()
+  route.params.id
+
+  const response = await findOne<Article>('articles', route.params.id, { populate: ["images"]})
+  const article = response.data.attributes
+  const imageURL = new URL(article.images.data[0].attributes.url, baseURL).toString()
+
+
 </script>
--- webpage/pages/index.vue
+++ webpage/pages/index.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
-    {{ response }}
-    <div></div>
-    <div class="bg-gray-500 h-32" v-for="resp in response.data">
-      <p>
-        {{ resp.attributes.title }}
-      </p>
-        {{ resp.attributes.text }}
-        <img :src="'http://localhost:1337' + resp.attributes.images.data[0].attributes.url" alt="">
+    <div v-for="resp in response.data">
+      <router-link class="text-blue-500" :to="`article/${ resp.id }`">
+        <p class="font-bold">
+          {{ resp.attributes.title }}
+        </p>
+      </router-link>
+        {{ resp.attributes.publishedAt }}
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
   const { find } = useStrapi()
-  const url = useStrapiUrl()
   const response = await find<Article>('articles', { populate: "images"})
 </script>
