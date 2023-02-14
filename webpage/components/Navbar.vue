8a8f308fd420510de729d584ef669b4fc6886b3f
Martin <martinfz@t-online.de>
2023-02-14 08:00:19 +0100

    Misc: Making Header text readable again

--- webpage/components/Navbar.vue
+++ webpage/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="h-12 text-black border-b-2 text-white text-4xl">
-    <router-link to="/">
+  <div class="h-12 border-b-2 text-white text-4xl">
+    <router-link class="text-black" to="/">
       Data is Awesome
     </router-link>
   </div>
