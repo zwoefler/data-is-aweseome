61e34d32abb5571a45e97467f69b42ec1155f8ee
Martin <martinfz@t-online.de>
2023-02-26 21:04:11 +0100

    Feature: new aggregated data

--- tesla_wait_time_isualization/aggregateModelData.py
+++ tesla_wait_time_isualization/aggregateModelData.py
@@ -15,45 +15,47 @@ def exportModelData(jsonData):
 ####################################################
 
 data_dir = "final_data"
-filename = "en_US_*.json"
+locales = ["zh_CN", "en_US", "de_DE", "no_NO"]
+for locale in locales:
+    filename = f"{locale}_*.json"
 
-json_files = glob.glob(os.path.join(data_dir, filename))
-locale = json_files[0][11:16]
+    json_files = glob.glob(os.path.join(data_dir, filename))
+    locale = json_files[0][11:16]
 
-exportFileName = f"aggregatedData_{locale}_model_data.json"
+    exportFileName = f"aggregatedData_{locale}_model_data.json"
 
-priceData = []
-exportData= {
-    "models": {}
-}
+    priceData = []
+    exportData= {
+        "models": {}
+    }
 
-for file_path in json_files:
-    with open(file_path, "r") as f:
-        importedJSON = json.load(f)
+    for file_path in json_files:
+        with open(file_path, "r") as f:
+            importedJSON = json.load(f)
 
-    print("FILE:", file_path)
-    trims = importedJSON["trims"]
-    model = importedJSON["model"]
+        print("FILE:", file_path)
+        trims = importedJSON["trims"]
+        model = importedJSON["model"]
 
-    if (model not in exportData["models"]):
-        exportData["models"][model] = {}
+        if (model not in exportData["models"]):
+            exportData["models"][model] = {}
 
-    for trimObj in trims:
-        if (trimObj["trimShorthandle"] not in exportData["models"][model]):
-            exportData["models"][model][trimObj["trimShorthandle"]] = {
-                "data": [],
-                "name": trimObj["displayedName"]
-            }
-        price = trimObj["price"]
-        date = importedJSON["date"]
-        exportData["models"][model][trimObj["trimShorthandle"]]["data"].append((date, price))
+        for trimObj in trims:
+            if (trimObj["trimShorthandle"] not in exportData["models"][model]):
+                exportData["models"][model][trimObj["trimShorthandle"]] = {
+                    "data": [],
+                    "name": trimObj["displayedName"]
+                }
+            price = trimObj["price"]
+            date = importedJSON["date"]
+            exportData["models"][model][trimObj["trimShorthandle"]]["data"].append((date, price))
 
 
-for model in exportData["models"].keys():
-    for trim, trimData in exportData["models"][model].items():
-        trimData["data"] = sorted(trimData["data"], key=lambda x: x[0])
+    for model in exportData["models"].keys():
+        for trim, trimData in exportData["models"][model].items():
+            trimData["data"] = sorted(trimData["data"], key=lambda x: x[0])
 
-metaData = importJSONFile(json_files[-1])["meta"]
-exportData["meta"] = metaData
+    metaData = importJSONFile(json_files[-1])["meta"]
+    exportData["meta"] = metaData
 
-exportModelData(exportData)
+    exportModelData(exportData)
