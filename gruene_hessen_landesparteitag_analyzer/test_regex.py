ef321d3b2748e53d144aa1832a9f9c4aba9ad18e
Martin <martinfz@t-online.de>
2023-02-28 09:56:46 +0100

    Misc: This regex works!

--- gruene_hessen_landesparteitag_analyzer/test_regex.py
+++ gruene_hessen_landesparteitag_analyzer/test_regex.py
@@ -1,22 +1,14 @@
 import re
+
 lines = [
     "Tarek Al-Wazir: 2:18:00 - 2:18:55",
-    "Angela Dorn: 1:17:52 - 1:25:20. Questions: 1:25:50 - 1:28:40"
+    "Angela Dorn: 1:17:52 - 1:25:20. Questions: 1:25:50 - 1:28:40",
 ]
 
-regex = r"^([\w\s]+?):\s+([\d:]+)\s*-\s*([\d:]+)(?:\.\s*(Questions?):\s*([\d:]+)\s*-\s*([\d:]+))?$"
+regex = re.compile(r'^([^:]+):\s*(\d+:\d+:\d+)\s*-\s*(\d+:\d+:\d+)(?:\.\s+([Qq]uestions?):?\s*(\d+:\d+:\d+)\s*-\s*(\d+:\d+:\d+))?')
 
-match = re.match(regex, line)
-
-if match:
-    name = match.group(1)
-    begin_speech = match.group(2)
-    end_speech = match.group(3)
-    has_questions = match.group(4) is not None
-    if has_questions:
-        questions_begin = match.group(5)
-        questions_end = match.group(6)
-    else:
-        questions_begin = None
-        questions_end = None
-    print(name, begin_speech, end_speech, has_questions, questions_begin, questions_end)
+for line in lines:
+    match = regex.match(line)
+    if match:
+        name, beginSpeech, endSpeech, question, beginQuestions, endQuestions = match.groups()
+        print(name, beginSpeech, endSpeech, question, beginQuestions, endQuestions)
