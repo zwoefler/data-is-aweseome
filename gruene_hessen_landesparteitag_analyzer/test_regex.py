f1d3ac95483422438d80a470af3f5e77c2aed726
Martin <martinfz@t-online.de>
2023-02-28 11:44:35 +0100

    MISC: Adding example for testing regex on markdown file

--- gruene_hessen_landesparteitag_analyzer/test_regex.py
+++ gruene_hessen_landesparteitag_analyzer/test_regex.py
@@ -1,14 +1,18 @@
 import re
 
-lines = [
-    "Tarek Al-Wazir: 2:18:00 - 2:18:55",
-    "Angela Dorn: 1:17:52 - 1:25:20. Questions: 1:25:50 - 1:28:40",
-]
 
-regex = re.compile(r'^([^:]+):\s*(\d+:\d+:\d+)\s*-\s*(\d+:\d+:\d+)(?:\.\s+([Qq]uestions?):?\s*(\d+:\d+:\d+)\s*-\s*(\d+:\d+:\d+))?')
+regex = re.compile(r'^'
+                   r'(?P<name>[^:]+):\s*'
+                   r'(?P<beginSpeech>\d+:\d+:\d+)\s*-\s*'
+                   r'(?P<endSpeech>\d+:\d+:\d+)'
+                   r'(?:\.\s+(?P<question>[Qq]uestions?)\s*:?\s*'
+                   r'(?P<beginQuestions>\d+:\d+:\d+)\s*-\s*'
+                   r'(?P<endQuestions>\d+:\d+:\d+))?')
 
-for line in lines:
-    match = regex.match(line)
-    if match:
-        name, beginSpeech, endSpeech, question, beginQuestions, endQuestions = match.groups()
-        print(name, beginSpeech, endSpeech, question, beginQuestions, endQuestions)
+with open("speeches.md", "r") as file:
+    lines = file.readlines()
+    for line in lines:
+        match = regex.match(line)
+        if match:
+            info = match.groupdict()
+            print(info)
