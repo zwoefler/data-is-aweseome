55137bddcaeea187c362113b00184ae4b5a3d38b
Martin <martinfz@t-online.de>
2023-03-08 23:08:09 +0100

    Misc: Script to download new Tesla data

---
+++ tesla_wait_time_isualization/get_new_data.sh
@@ -0,0 +1,13 @@
+python3 saveWebArchiveTeslaURLs.py
+
+for file in wayback_LinkLists/*.json
+do
+  # Run the downloadHistoricHTML.py script with the current file as an argument
+  python3 downloadHistoricHTML.py "$file"
+done
+
+python3 extractJSONFromHTML.py
+
+python3 getTeslaModelData.py
+
+python3 aggregateModelData.py
