8f427956fddc431850429685d065bffe748cf776
Martin <martinfz@t-online.de>
2023-04-10 21:22:58 +0200

    WEBPAGE: Misc: Reworking Webpage. Starting from zero

--- webpage/pages/index.vue
+++ webpage/pages/index.vue
@@ -1,32 +1,23 @@
 <template>
   <div>
-    <div v-for="resp in response.data">
-      <router-link class="text-blue-500" :to="`article/${ resp.id }`">
-        <p class="font-bold">
-          {{ resp.attributes.title }}
-        </p>
-      </router-link>
-      {{ formatDate(resp.attributes.publishedAt) }}
-
-    </div>
+    INDEX PAGE
+    <p>Data Analysis</p>
+    <p v-for="project in projects">
+      {{ project.project }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-  const { find } = useStrapi()
-  const response = await find<Article>('articles', { populate: "images"})
+interface Project {
+  project: string;
+  link:string;
+}
 
-  function formatDate(iso8601_date) {
-    const date = new Date(iso8601_date);
-    const options = {
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric'
-    };
-    const formattedDate = date.toLocaleDateString('de-DE', options)
-
-    return formattedDate
-  }
+  const projects: Object[] = [
+    {
+      project: "Abiturnoten",
+      link: "/abiturnoten"
+    }
+  ]
 </script>
