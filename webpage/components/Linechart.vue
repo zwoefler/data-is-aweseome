7c49217dffe8780823418b3b01295b52b9ca0dd4
Martin <martinfz@t-online.de>
2023-04-11 12:10:18 +0200

    Misc: Making LineChart in Webpage work with props!

--- webpage/components/Linechart.vue
+++ webpage/components/Linechart.vue
@@ -1,11 +1,10 @@
 <template>
-  <Line :data="chartData" :options="chartOptions" />
+  <Line :data="props.chartData" :options="props.chartOptions" />
 </template>
 
 
 <script setup lang="ts">
 import { Line } from 'vue-chartjs'
-import grades_data from 'assets/abitur_grades.json'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -27,30 +26,16 @@ ChartJS.register(
   Legend
 )
 
-    var chartData = {
-      labels: [],
-      datasets: [
-        {
-          label: 'Durchschnittliche Abiturnoten Deutschland 2006 - 2022',
-          backgroundColor: '#f87979',
-          data: []
-        },
-      ],
-    }
 
-    var chartOptions = {
-      responsive: true,
-      maintainAspectRatio: true,
-      scales: {
-        y: {
-          min: 1.0,
-          max: 4.0
-        }
-      }
-    }
+const props = defineProps({
+  chartOptions: {
+    type: Object,
+    required: true
+  },
+  chartData: {
+    type: Object,
+    required: true
+  }
+})
 
-    for (const key in grades_data) {
-      chartData["labels"].push(grades_data[key]["year"])
-      chartData["datasets"][0]["data"].push(grades_data[key]["average_grade"])
-    }
 </script>
--- webpage/pages/abiturnoten.vue
+++ webpage/pages/abiturnoten.vue
@@ -1,6 +1,39 @@
 <template>
   <div>
     <h1>Abiturnoten</h1>
-    <Linechart></Linechart>
+    <Linechart :chartData="chartData" :chartOptions="chartOptions"></Linechart>
   </div>
 </template>
+
+<script setup lang="ts">
+import grades_data from 'assets/abitur_grades.json'
+
+var chartData = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Durchschnittliche Abiturnoten Deutschland 2006 - 2022',
+        backgroundColor: '#f87979',
+        data: []
+      },
+    ],
+  }
+
+  var chartOptions = {
+    responsive: true,
+    maintainAspectRatio: true,
+    scales: {
+      y: {
+        min: 1.0,
+        max: 4.0
+      }
+    }
+  }
+
+for (const key in grades_data) {
+  chartData["labels"].push(grades_data[key]["year"])
+  chartData["datasets"][0]["data"].push(grades_data[key]["average_grade"])
+}
+
+
+</script>
