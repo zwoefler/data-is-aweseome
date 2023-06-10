9c6d391c7c6456b822b741f1da6d06bf76ff531a
Martin <martinfz@t-online.de>
2023-06-10 11:10:05 +0200

    FEATURE: Adding Graph for OECD 2023 forecast

---
+++ oecd_gdp_growth_forecast/README.md
@@ -0,0 +1,18 @@
+# OECD Forecast Data for ALL countries
+The OECD has 38 member states and recently released the forecast for their member countries.
+
+## Visualize 2023 Forecast for ALL countries
+The data is in Percentage, comapred to the last year.
+
+https://oecd.org/economic-outlook/june-2023/
+
+
+Get a graph where all the countries are aligned on the Y-Axis
+- Positive values to the right, negative to the left
+- 
+
+## ToDOs
+1. Get Data from some URL
+2. Organizie to only have GDP growth forecast data for 38 member countries
+3. Recreate visuals from Forecast graph from link above
+
---
+++ oecd_gdp_growth_forecast/visualize_OECD_2023_real_gdp_forecast.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file_path = "OECD-economic-outlook-june-2023-GDP-data.xlsx"
+sheet_name = "Data"
+start_cell = "B7"
+end_cell = "M57"
+
+df = pd.read_excel(file_path, sheet_name=sheet_name, header=None, skiprows=6, usecols="B:M")
+df.columns = df.iloc[0]
+df = df.iloc[1:]
+
+value_column = 2023.0
+label_column = "Country EN"
+
+df_sorted = df.sort_values(value_column, ascending=True)
+
+fig, ax = plt.subplots(figsize=(15,20))
+
+bars = ax.barh(df_sorted[label_column], df_sorted[value_column])
+ax.set_title("OECD 2023 Real GDP Growth Forecast", fontsize=20)
+ax.set_xlabel("Real GDP Forecast 2023 by OECD", fontsize=16)
+ax.set_ylabel("Countries", fontsize=16)
+
+for i, value in enumerate(df_sorted[value_column]):
+    ax.text(value, i, "{:.2f}".format(value), ha='left', va='center', fontsize=12)
+
+for bar, label, economy in zip(bars, df_sorted[label_column], df_sorted["Economy"]):
+    if "World" in label:
+        bar.set_color('gray')
+    if "OECD" in label:
+        bar.set_color('gray')
+    if "Euro area" in label:
+        bar.set_color('gray')
+    elif "OECD countries" in economy:
+        bar.set_color('orange')
+
+# Adjust max x value to fit largest number on chart
+max_value = df_sorted[value_column].max()
+ax.set_xlim(right=max_value * 1.5)
+
+plt.show()
