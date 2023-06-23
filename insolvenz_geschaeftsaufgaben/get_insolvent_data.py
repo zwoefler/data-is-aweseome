d764e0843f13e5498d3f667375defe297ea95cad
Martin <martinfz@t-online.de>
2023-06-23 14:59:12 +0200

    FEATURE: Adding month ticks to the graph

--- insolvenz_geschaeftsaufgaben/get_insolvent_data.py
+++ insolvenz_geschaeftsaufgaben/get_insolvent_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from datetime import datetime
 import locale
 
@@ -28,9 +29,12 @@ plt.plot(date_column, insolvencies)
 plt.title("Insolvenzen Deutschland")
 plt.xlabel("Months")
 plt.ylabel("Insolvencies (total)")
+
+# Configure the x-axis ticks and labels
+plt.gca().xaxis.set_major_locator(mdates.YearLocator())  # Set the tick locations to every year
+plt.gca().xaxis.set_minor_locator(mdates.MonthLocator())  # Set the tick locations to every month
+plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter("%b"))  # Format the minor tick labels to display month abbreviation
+plt.gca().tick_params(axis='x', which='major', length=0, labelsize=10)  # Hide the major tick labels
+plt.gca().tick_params(axis='x', which='minor', length=2, labelsize=7, rotation=90)  # Set the length of the minor ticks
+
 plt.show()
-
-
-
-
-
