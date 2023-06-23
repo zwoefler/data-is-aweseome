efb0864ea6786f5e71fcb3136be37b37826bdfd0
Martin <martinfz@t-online.de>
2023-06-23 15:02:24 +0200

    MISC: Restructuring code, making functions renaming file for better readability

---
+++ insolvenz_geschaeftsaufgaben/visualize_germany_insolvency_data.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import json
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
+# insolvenz_url = "https://www.destatis.de/DE/Themen/Branchen-Unternehmen/Unternehmen/Gewerbemeldungen-Insolvenzen/Tabellen/Insolvenzen.html#fussnote-1-241898"
+
+def export_df_to_JSON(data, json_file_name):
+    with open(json_file_name, 'w') as f:
+        json.dump(data, f)
+
+
+def convert_month_year_date_to_datetime_object(date_string, date_format="%Y %b"):
+    date = datetime.strptime(date_string, date_format)
+    return date
+
+
+def visualize_data(x_axis, y_axis, title, x_label, y_label):
+    plt.plot(x_axis, y_axis)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+
+    plt.gca().xaxis.set_major_locator(mdates.YearLocator())
+    plt.gca().xaxis.set_minor_locator(mdates.MonthLocator())
+    plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter("%b"))
+    plt.gca().tick_params(axis='x', which='major', length=0, labelsize=10)
+    plt.gca().tick_params(axis='x', which='minor', length=2, labelsize=7, rotation=90)
+
+    plt.show()
+
+
+data = pd.read_html("insolvenzen.html")[0][:-1]
+date_column = data[data.columns[0]] + " " + data[data.columns[1]]
+date_column = date_column.apply(lambda x: convert_month_year_date_to_datetime_object(x)).tolist()
+insolvencies = data[data.columns[2]].apply(lambda x: int("".join(x.split()))).tolist()
+
+
+
+
+
+visualize_data(date_column, insolvencies, title="Insolvenzen Deutschland", x_label="Months", y_label="Insolvencies (total)")
