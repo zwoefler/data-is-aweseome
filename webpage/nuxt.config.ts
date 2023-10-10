fee39c7404f3a06751cbeb169c6163c7d871b692
Martin <martinfz@t-online.de>
2023-10-10 15:30:56 +0200

    MISC: Removed strapi from config, added content module

--- webpage/nuxt.config.ts
+++ webpage/nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: [
-        '@nuxtjs/strapi',
+        '@nuxt/content',
         '@nuxtjs/tailwindcss'
     ],
-    strapi: {
-        url: 'http://localhost:1337'
-      },
 })
