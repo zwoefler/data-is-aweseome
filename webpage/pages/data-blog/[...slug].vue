a7bc42c1c4dae8a1f8283a73017b427ef43694fa
Martin <martinfz@t-online.de>
2023-10-11 18:32:42 +0200

    MISC: making text prettier in blog

--- webpage/pages/data-blog/[...slug].vue
+++ webpage/pages/data-blog/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
   <main class="w-full">
     <ContentDoc v-slot="{ doc }">
-      <article class="prose">
+      <article class="prose prose-lg md:prose-xl">
         <ContentRenderer :value="doc">
         </ContentRenderer>
       </article>
