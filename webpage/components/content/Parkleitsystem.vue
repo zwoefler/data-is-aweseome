f65b3900c49df66f66398e3b7878380a0013d5cf
Martin <martinfz@t-online.de>
2023-11-04 16:26:43 +0100

    MISC: Adding parkleitsystem as draft

---
+++ webpage/components/content/Parkleitsystem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-full">
+    <div>
+      <select v-model="selectedOption">
+        <option v-for="option in selectionOptions" :key="option" :value="option">{{ option }}</option>
+      </select>
+    </div>
+    <Linechart :chartOptions="chartOptions" :labels="labels" :data="data" :label="label"></Linechart>
+  </div>
+</template>
+
+<script setup lang="ts">
+import parkleitsystem from 'assets/parkleitsystem.json'
+
+
+
+</script>
---
+++ webpage/content/data-blog/parkleitsystem.md
@@ -0,0 +1,8 @@
+---
+draft: true
+---
+# Parkleitsystem Gießen
+
+Das Parkleitsystem in Gießen visualisiert.
+
+:parkleitsystem
