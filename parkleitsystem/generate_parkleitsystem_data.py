5ee9311a4153db6258c22540af7e5bff3b1a77ae
Martin <martinfz@t-online.de>
2023-11-04 16:47:53 +0100

    DEV: DOnt add existing data

--- parkleitsystem/generate_parkleitsystem_data.py
+++ parkleitsystem/generate_parkleitsystem_data.py
@@ -1,2 +1,13 @@
 def add_new_data(existing_data, new_data):
-    return []
+    final_data = existing_data
+    message = ""
+
+    timestamp = new_data["timestamp"]
+
+    if new_data not in existing_data:
+        final_data.append(new_data)
+        message = f"Added {timestamp}"
+    else:
+        message = f"Skipped {timestamp}, it already exists in the dataset"
+
+    return final_data, message
