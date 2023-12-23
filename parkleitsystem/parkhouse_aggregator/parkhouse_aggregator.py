53d85d6f0c23698adb1daac44ea5f22fb5050288
Martin <martinfz@t-online.de>
2023-12-24 00:56:54 +0100

    Adding parkhouse_timestmap to epoch seconds

--- parkleitsystem/parkhouse_aggregator/parkhouse_aggregator.py
+++ parkleitsystem/parkhouse_aggregator/parkhouse_aggregator.py
@@ -1,11 +1,24 @@
 import os
+from datetime import datetime, timezone
 
 
 def aggregate_parkhouse_data(data_dir):
     return
 
 
-def create_parkhouse_data_folder(data_dir):
+def list_files_in_directory(data_dir):
+    return []
+
+
+def convert_timestamp_to_epoch_seconds(timestamp):
+    date_format = "%d%m%Y-%H%M"
+    date_object = datetime.strptime(timestamp, date_format)
+    epoch_seconds = int(date_object.replace(tzinfo=timezone.utc).timestamp())
+
+    return epoch_seconds
+
+
+def create_parkhouse_data_folder(data_dir="parkhouse_data"):
     module_dir = os.path.dirname(os.path.dirname(__file__))
 
     parkhouse_data_dir = os.path.join(module_dir, data_dir)
--- parkleitsystem/tests/test_parkhouse_aggregator.py
+++ parkleitsystem/tests/test_parkhouse_aggregator.py
@@ -2,14 +2,18 @@ import unittest
 import os
 import tempfile
 
-from parkhouse_aggregator.parkhouse_aggregator import create_parkhouse_data_folder
+from parkhouse_aggregator.parkhouse_aggregator import (
+    create_parkhouse_data_folder,
+    list_files_in_directory,
+    convert_timestamp_to_epoch_seconds,
+)
 
 
 class FileSystemTests(unittest.TestCase):
     def test_create_parkhouse_data_folder(self):
         parent_directory = os.path.dirname(os.path.dirname(__file__))
-        parkhouse_data_dir = "fake_parkhouse_dir"
-        create_parkhouse_data_folder(parkhouse_data_dir)
+        parkhouse_data_dir = "parkhouse_data"
+        create_parkhouse_data_folder()
 
         self.assertTrue(
             os.path.exists(os.path.join(parent_directory, parkhouse_data_dir))
@@ -19,6 +23,19 @@ class FileSystemTests(unittest.TestCase):
         )
 
         os.rmdir(parkhouse_data_dir)
+
+    def test_get_file_list_from_data_folder(self):
+        data_dir = "data/"
+        file_list = list_files_in_directory(data_dir)
+
+        self.assertIsInstance(file_list, list)
+
+
+class TestHelperFunctions(unittest.TestCase):
+    def test_convert_timestamp_from_custom_to_epoch_seconds(self):
+        epoch_timestamp = convert_timestamp_to_epoch_seconds("09112023-0900")
+
+        self.assertEqual(epoch_timestamp, 1699520400)
 
 
 if __name__ == "__main__":
