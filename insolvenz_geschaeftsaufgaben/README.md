d8172da243c165d1178374d410459f70c7eb3407
Martin <martinfz@t-online.de>
2023-12-28 01:42:05 +0100

    Update Insolenvies in Germany

--- insolvenz_geschaeftsaufgaben/README.md
+++ insolvenz_geschaeftsaufgaben/README.md
@@ -1,5 +1,31 @@
 # Insolvenzen und Geschaeftsaufgaben in Deutschland
 
+In late 2022 some germans had more worries about an "Insolvency wave" during the winter.
+There was an interview with german minster of economy `Robert Habeck` saying companies will stop producing, but now go insolvent.
+
+Now, there was very little context, that's why we will look at the insolvency rates AND at the rates for people giving up their businesses.
+
+
+## 🏗️ Usage
+
+```BASH
+python3 -m venv Env
+source Env/bin/activate
+
+python3 insolvency_in_germany/visualize_germany_insolvency_data.py
+```
+
+---
+
+Last run December 2023.
+Data until September 2023
+![](data/Insolvencies_in_Germany.png)
+
+## 🚧 Problems
+- Add legend to graphs!
+-
+
+
 1. Insolvenzen URL:https://www.destatis.de/DE/Themen/Branchen-Unternehmen/Unternehmen/Gewerbemeldungen-Insolvenzen/Tabellen/Insolvenzen.html#241898
 Insolvenzen seit 1953: https://www.destatis.de/DE/Themen/Branchen-Unternehmen/Unternehmen/Gewerbemeldungen-Insolvenzen/Tabellen/lrins01.html#242428
 
--- insolvenz_geschaeftsaufgaben/insolvency_in_germany/visualize_germany_insolvency_data.py
+++ insolvenz_geschaeftsaufgaben/insolvency_in_germany/visualize_germany_insolvency_data.py
@@ -6,10 +6,10 @@ from datetime import datetime
 import locale
 
 locale.setlocale(locale.LC_TIME, "de_DE.utf-8")
-# insolvenz_url = "https://www.destatis.de/DE/Themen/Branchen-Unternehmen/Unternehmen/Gewerbemeldungen-Insolvenzen/Tabellen/Insolvenzen.html#fussnote-1-241898"
+
 
 def export_df_to_JSON(data, json_file_name):
-    with open(json_file_name, 'w') as f:
+    with open(json_file_name, "w") as f:
         json.dump(data, f)
 
 
@@ -18,9 +18,9 @@ def convert_month_year_date_to_datetime_object(date_string, date_format="%Y %b")
     return date
 
 
-def visualize_data(x_axis, data, title, x_label, y_label):
-    for data_column in data:
-        plt.plot(x_axis, data_column)
+def visualize_data(x_axis, data, title, x_label, y_label, legend_labels):
+    for i, data_column in enumerate(data):
+        plt.plot(x_axis, data_column, label=legend_labels[i])
     plt.title(title)
     plt.xlabel(x_label)
     plt.ylabel(y_label)
@@ -28,21 +28,31 @@ def visualize_data(x_axis, data, title, x_label, y_label):
     plt.gca().xaxis.set_major_locator(mdates.YearLocator())
     plt.gca().xaxis.set_minor_locator(mdates.MonthLocator())
     plt.gca().xaxis.set_minor_formatter(mdates.DateFormatter("%b"))
-    plt.gca().tick_params(axis='x', which='major', length=0, labelsize=10)
-    plt.gca().tick_params(axis='x', which='minor', length=2, labelsize=7, rotation=90)
+    plt.gca().tick_params(axis="x", which="major", length=0, labelsize=10)
+    plt.gca().tick_params(axis="x", which="minor", length=2, labelsize=7, rotation=90)
 
+    plt.legend()
     plt.show()
 
 
-data = pd.read_html("insolvenzen.html")[0][:-1]
+insolvenz_url = "https://www.destatis.de/DE/Themen/Branchen-Unternehmen/Unternehmen/Gewerbemeldungen-Insolvenzen/Tabellen/Insolvenzen.html#fussnote-1-241898"
+data = pd.read_html(insolvenz_url)[0][:-1]
 date_column = data[data.columns[0]] + " " + data[data.columns[1]]
-date_column = date_column.apply(lambda x: convert_month_year_date_to_datetime_object(x)).tolist()
+date_column = date_column.apply(
+    lambda x: convert_month_year_date_to_datetime_object(x)
+).tolist()
 insolvencies = data[data.columns[2]].apply(lambda x: int("".join(x.split()))).tolist()
 companies = data[data.columns[3]].apply(lambda x: int("".join(x.split()))).tolist()
 consumer = data[data.columns[4]].apply(lambda x: int("".join(x.split()))).tolist()
 
-
 data = [insolvencies, companies, consumer]
+legend_labels = ["Insolvencies", "Companies", "Consumer"]
 
-
-visualize_data(date_column, data, title="Insolvenzen Deutschland", x_label="Months", y_label="Insolvencies (total)")
+visualize_data(
+    date_column,
+    data=data,
+    title="Insolvenzen Deutschland",
+    x_label="Months",
+    y_label="Insolvencies (total)",
+    legend_labels=legend_labels,
+)
