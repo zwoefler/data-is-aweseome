9646ed9465a04837b4882304e030fa47c4200985
Martin <martinfz@t-online.de>
2024-01-14 18:53:16 +0100

    Updating Articles view

--- WEBPAGE/pages/index.vue
+++ WEBPAGE/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="space-y-2">
+    <h1 class="text-3xl font-bold">ARTIKEL:</h1>
     <div class="flex flex-col">
       <router-link class="p-2 flex flex-col border border-2 border-gray-600/20" :to="blog._path" v-for="blog in data">
         <p class="font-bold text-xl">{{ blog.title }}</p>
