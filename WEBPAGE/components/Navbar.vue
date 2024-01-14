703d7525cb1fcf984e5c1715e10b4fa0ab766001
Martin <martinfz@t-online.de>
2024-01-14 18:19:38 +0100

    Centering webpage name

--- WEBPAGE/components/Navbar.vue
+++ WEBPAGE/components/Navbar.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="h-12 text-4xl">
-    <router-link to="/">
-      Daten sind GEIL!
-    </router-link>
+    <div class="flex items-center justify-center">
+      <router-link class="
+            font-bold
+            p-2
+            text-xl
+            md:text-4xl text-white font-gruene-semibold
+            uppercase w-max" to="/">
+        Daten sind GEIL!
+      </router-link>
+
+    </div>
+
+
   </div>
 </template>
