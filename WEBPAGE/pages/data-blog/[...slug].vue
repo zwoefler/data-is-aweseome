aca8baafa2fbbb80922cf2bf5dbbb0511df4eba3
Martin <martinfz@t-online.de>
2024-01-28 15:57:29 +0100

    Fix overflow when article is too wide

--- WEBPAGE/pages/data-blog/[...slug].vue
+++ WEBPAGE/pages/data-blog/[...slug].vue
@@ -1,7 +1,7 @@
 <template>
   <main class="w-full flex justify-center">
     <ContentDoc v-slot="{ doc }">
-      <article class="prose md:prose-xl prose-invert">
+      <article class="prose md:prose-xl prose-invert w-full">
         <ContentRenderer :value="doc">
         </ContentRenderer>
       </article>
