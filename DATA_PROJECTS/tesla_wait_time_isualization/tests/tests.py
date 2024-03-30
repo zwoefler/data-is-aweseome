0239c3be2bb6828738f55bf5c134bca3585edeba
Martin <martinfz@t-online.de>
2024-03-30 14:01:18 +0100

    Adding json smoketest

--- DATA_PROJECTS/tesla_wait_time_isualization/tests/tests.py
+++ DATA_PROJECTS/tesla_wait_time_isualization/tests/tests.py
@@ -1,6 +1,21 @@
 import unittest
 
+from tesla_price_visualizer import extract_json_from_html
 
-class SmokeTest(unittest.TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+
+class ExtractJSONFromHTML(unittest.TestCase):
+    def test_extract_JSON_from_HTML(self):
+        html_content = """
+            <hmtl>
+                <body>
+                    <div id="json_data">
+                        {"example_key": "example_value"}
+                    </div>
+                </body>
+            </html>
+        """
+
+        json_data = extract_json_from_html(html_content)
+
+        expected_json = {"example_key": "example_value"}
+        self.assertEqual(json_data, expected_json)
