01c645af7a7f0069624ba0f6f07006ee78dde859
Martin <martinfz@t-online.de>
2024-05-20 12:20:33 +0200

    Refactor getting dataframe from HTL table "

--- DATA_PROJECTS/tesla_deliveries/tesla_deliveries.py
+++ DATA_PROJECTS/tesla_deliveries/tesla_deliveries.py
@@ -5,15 +5,9 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 
-def extract_totals_from_table(html_table):
-    """Extracts the total production number from table in press release"""
-    totals = {"production": 0, "deliveries": 0}
-
-    pandas_html_import = pd.read_html(html_table)
-    df = pandas_html_import[0]
-    totals["deliveries"] = int(df[2][3])
-    totals["production"] = int(df[1][3])
-    return totals
+def fetch_html(url):
+    html = requests.get(url)
+    return html.text
 
 
 def extract_table(html):
@@ -23,15 +17,21 @@ def extract_table(html):
     return table
 
 
+def extract_totals_from_table(html_table):
+    """Extracts the total production number from HTML table"""
+    totals = {"production": 0, "deliveries": 0}
+
+    df = pd.read_html(html_table)[0]
+
+    totals["deliveries"] = int(df[2][3])
+    totals["production"] = int(df[1][3])
+    return totals
+
+
 def extract_totals(html):
     html_table = extract_table(html)
     totals = extract_totals_from_table(html_table)
     return totals
-
-
-def fetch_html(url):
-    html = requests.get(url)
-    return html.text
 
 
 def get_numbers_from_press_release(url):
