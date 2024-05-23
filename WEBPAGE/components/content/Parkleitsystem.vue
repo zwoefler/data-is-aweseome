b5a9f5925d15815e3f793ccb799cb27eca1fa7f6
Martin <martinfz@t-online.de>
2024-05-23 09:24:09 +0200

    Update Parkleitsystem data in webpage

--- WEBPAGE/components/content/Parkleitsystem.vue
+++ WEBPAGE/components/content/Parkleitsystem.vue
@@ -1,114 +1,147 @@
 <template>
-    <div class="w-full">
-        <p class="font-bold text-2xl">{{ parkhouse }}</p>
-        <p>{{ shortDate(selectedWeekStart) }} - {{ shortDate(selectedWeekEnd) }}</p>
-        <Line :data="chartDataSet" :options="chartOptions" />
+  <div class="w-full">
+    <p class="font-bold text-2xl">{{ parkhouse }}</p>
+    <p>{{ shortDate(selectedWeekStart) }} - {{ shortDate(selectedWeekEnd) }}</p>
+    <div class="my-4">
+      <button @click="previousWeek" class="bg-blue-500 text-white px-4 py-2 rounded">Previous Week</button>
+      <button @click="nextWeek" class="bg-blue-500 text-white px-4 py-2 rounded ml-2">Next Week</button>
     </div>
+    <Line :data="chartDataSet" :options="chartOptions" />
+  </div>
 </template>
 
 <script setup>
 import { Line } from 'vue-chartjs'
-import { startOfISOWeek, endOfISOWeek } from 'date-fns';
-import parkleitsystem from 'assets/parkhouses/am bahnhof_data_20112023.json'
+import { startOfISOWeek, endOfISOWeek, subWeeks, addWeeks } from 'date-fns';
+import parkleitsystem from 'assets/parkhouses/am bahnhof.json'
 import { ref } from 'vue';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 } from 'chart.js'
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 )
 
 
 const jsonData = parkleitsystem
 
-const parkhouse = ref(jsonData[0].name)
+const parkhouse = ref(jsonData.name)
 
 var chartLabels = ref([])
 var chartData = ref([])
 
 var chartDataSet = ref({
-    labels: chartLabels.value, // Time (x-axis)
-    datasets: [
-        {
-            label: 'Occupied Spaces',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            data: chartData.value, // Occupied Spaces (y-axis)
-        },
-    ],
-});
-var chartOptions = ref({
-    scales: {
-        xAxes: [
-            {
-                type: 'time',
-                time: {
-                    unit: 'hour',
-                },
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Time',
-                },
-            },
-        ],
-        yAxes: [
-            {
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Besetzte Parkplätze',
-                },
-            },
-        ],
+  labels: chartLabels.value, // Time (x-axis)
+  datasets: [
+    {
+      label: 'Occupied Spaces',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      data: chartData.value, // Occupied Spaces (y-axis)
     },
+  ],
+});
+
+var chartOptions = ref({
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          unit: 'day',
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Time',
+        },
+      },
+    ],
+    yAxes: [
+      {
+        scaleLabel: {
+          display: true,
+          labelString: 'Besetzte Parkplätze',
+        },
+      },
+    ],
+  },
 })
 
 
 const shortDate = (date) => {
-    var options = { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' }
-    return date.toLocaleDateString("de-DE", options)
+  var options = { weekday: 'short', year: 'numeric', month: 'numeric', day: 'numeric' }
+  return date.toLocaleDateString("de-DE", options)
 }
 
 
 let selectedWeekStart = ref(startOfISOWeek(new Date()));
 let selectedWeekEnd = ref(endOfISOWeek(selectedWeekStart.value))
 
+const previousWeek = () => {
+  selectedWeekStart.value = subWeeks(selectedWeekStart.value, 1)
+  selectedWeekEnd.value = endOfISOWeek(selectedWeekStart.value)
+  updateChart()
+}
+
+const nextWeek = () => {
+  selectedWeekStart.value = addWeeks(selectedWeekStart.value, 1)
+  selectedWeekEnd.value = endOfISOWeek(selectedWeekStart.value)
+  updateChart()
+}
+
 
 watch([chartLabels, chartData], ([newChartLabel, newChartData]) => {
-    chartDataSet.value = {
+  chartDataSet.value = {
     labels: newChartLabel, // Time (x-axis)
     datasets: [
-        {
-            label: 'Occupied Spaces',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            data: newChartData, // Occupied Spaces (y-axis)
-        },
+      {
+        label: 'Occupied Spaces',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        data: newChartData, // Occupied Spaces (y-axis)
+      },
     ],
-    }
+  }
 })
 
 const updateChart = () => {
-    var updatedChartData = updateChartData(jsonData, selectedWeekStart.value)
-    chartLabels.value = updatedChartData.labels
-    chartData.value = updatedChartData.data
+  var updatedChartData = extractChartData(jsonData.occupation_data, selectedWeekStart.value, selectedWeekEnd.value)
+  chartLabels.value = updatedChartData.labels
+  chartData.value = updatedChartData.occupiedSpaces
+}
+
+const extractChartData = (data, weekStart, weekEnd) => {
+  const labels = []
+  const occupiedSpaces = []
+
+  data.forEach(entry => {
+    const entryDate = new Date(entry.timestamp * 1000)
+    if (entryDate >= weekStart && entryDate <= weekEnd) {
+      labels.push(entryDate)
+      occupiedSpaces.push(entry.occupied_spaces)
+    }
+  })
+
+  return {
+    labels,
+    occupiedSpaces,
+  }
 }
 
 updateChart()
-
-
 
 </script>
