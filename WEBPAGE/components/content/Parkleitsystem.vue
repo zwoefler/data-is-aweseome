4dc40c6bbf6881e9a8f6a353891712167053d094
zwoefler <>
2024-09-21 15:27:44 +0200

    rmoving background color from data lines

--- WEBPAGE/components/content/Parkleitsystem.vue
+++ WEBPAGE/components/content/Parkleitsystem.vue
@@ -188,13 +188,11 @@ const updateChart = () => {
       {
         label: 'Besetze Parkplätze',
         borderColor: 'rgba(255, 122, 0, 1)',
-        backgroundColor: 'rgba(255, 122, 0, 0.2)',
         data: updatedChartData.value.occupiedSpaces, // Occupied Spaces (y-axis)
       },
       {
         label: 'Verfügbare Plätze',
         borderColor: 'rgba(75, 192, 192, 1)',
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
         data: updatedChartData.value.maxSpaces, // Occupied Spaces (y-axis)
       },
     ],
