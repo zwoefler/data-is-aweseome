fd69d6953aa232f4cca89553bc151e7486d92e65
zwoefler <>
2024-09-21 15:25:25 +0200

    NO data available message over dummy graph

--- WEBPAGE/components/content/Parkleitsystem.vue
+++ WEBPAGE/components/content/Parkleitsystem.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="w-full p-4 bg-gray-900">
-    <div class="mb-4">
-      <label for="parkhouse-select" class="text-white">Select Parkhouse:</label>
-      <select id="parkhouse-select" v-model="selectedParkhouse" @change="updateChart" class="ml-2 p-2 rounded bg-white">
+  <div class="w-full p-4 bg-gray-900 space-y-4">
+    <div class="space-y-1">
+      <label for="parkhouse-select" class="text-white">Wähle ein Parkhaus:</label>
+      <select id="parkhouse-select" v-model="selectedParkhouse" @change="updateChart"
+        class="bg-blue-500 text-xs font-bold text-white p-2 rounded ml-2">
         <option v-for="parkhouse in parkhouses" :key="parkhouse.name" :value="parkhouse">{{ parkhouse.name }}
         </option>
       </select>
-    </div>
-    <div>
-      <p class="font-bold text-2xl">{{ selectedParkhouse.name }}</p>
-      <p>{{ shortDate(selectedWeekStart) }} - {{ shortDate(selectedWeekEnd) }}</p>
+      <div class="font-bold text-2xl">{{ selectedParkhouse.name }}</div>
+      <div>{{ shortDate(selectedWeekStart) }} - {{ shortDate(selectedWeekEnd) }}</div>
       <div class="space-x-2">
         <button @click="previousWeek" :disabled="isPreviousWeekDisabled"
           class="disabled:bg-gray-700 disabled:text-gray-300 bg-blue-500 text-xs text-white p-2 py-2 rounded">Letzte
@@ -19,10 +18,12 @@
           Woche</button>
       </div>
     </div>
-    <div class="h-80 w-full">
+    <div class="w-full relative">
       <p v-if="!chartDataSet" class="bg-orange-500 text-black font-xl">LOADING DATA</p>
-      <p v-if="noData">NO DATA AVAILABLE FOR SELECTED TIMEFRAME</p>
       <Line v-else class="h-full" :data="chartDataSet" :options="chartOptions" />
+      <p v-if="noData"
+        class="absolute top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/3 inset-x-0 p-2 bg-orange-500 text-xs text-white font-bold text-center">
+        KEINE DATEN FÜR DIESEN ZEITRAUM</p>
     </div>
   </div>
 </template>
@@ -167,6 +168,17 @@ const updateChart = () => {
   updatedChartData.value = extractChartData(selectedParkhouse.value.occupation_data, selectedWeekStart.value, selectedWeekEnd.value)
   if (updatedChartData.value.occupiedSpaces.length < 1) {
     noData.value = true
+    chartDataSet.value = {
+      labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], // Time (x-axis)
+      datasets: [
+        {
+          label: '',
+          borderColor: 'rgba(255, 122, 0, 0.2)',
+          data: [0, 1, 5, 2, 4, 6, 6, 7, 3, 2, 0], // Occupied Spaces (y-axis)
+        }
+      ],
+    }
+    return
   } else {
     noData.value = false
   }
