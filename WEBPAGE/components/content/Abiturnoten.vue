6ee6d85cbf3d130c55dc6888170d5ee00fdb915c
zwoefler <>
2024-09-25 12:28:52 +0200

    Prettifying Abiturnoten

--- WEBPAGE/components/content/Abiturnoten.vue
+++ WEBPAGE/components/content/Abiturnoten.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="w-full">
     <div>
-      <select v-model="selectedOption">
+      <label for="parkhouse-select" class="text-white">Wähle ein Bundesland:</label>
+      <select id="parkhouse-select" v-model="selectedOption"
+        class="bg-blue-500 text-xs font-bold text-white p-2 rounded ml-2">
         <option v-for="option in selectionOptions" :key="option" :value="option">{{ option }}</option>
       </select>
     </div>
@@ -20,9 +22,28 @@ var chartOptions = ref({
   maintainAspectRatio: true,
   scales: {
     y: {
+      ticks: {
+        color: 'rgba(255, 255, 255, 1)',
+      },
       min: 1.0,
-      max: 4.0
+      max: 3.0
+    },
+    x: {
+      ticks: {
+        color: 'rgba(255, 255, 255, 1)',
+      },
+      title: {
+        display: true,
+        color: 'rgba(255, 255, 255, 1)',
+      }
     }
+  },
+  plugins: {
+    legend: {
+      labels: {
+        color: 'rgba(255, 255, 255, 1)'
+      }
+    },
   }
 })
 
@@ -31,19 +52,19 @@ var selectionOptions = ref(["Total"])
 labels.value = grades_data["years"]
 data.value = grades_data["average_grade"][selectedOption.value]
 
-for (const key in grades_data[2006]["states"]){
+for (const key in grades_data[2006]["states"]) {
   selectionOptions.value.push(key)
 }
 
 
-  watch(
+watch(
   () => selectedOption.value,
   () => {
     changeSelection(selectedOption.value)
   }
 )
 
-function changeSelection(selection: string){
+function changeSelection(selection: string) {
   data.value = []
   data.value = grades_data["average_grade"][selection]
 }
