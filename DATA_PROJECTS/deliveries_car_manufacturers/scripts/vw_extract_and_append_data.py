f158dcae8814af4d52b411056b575998ab06a6c2
zwoefler <>
2024-10-29 08:47:45 +0100

    All China mentioning rows are now just China

--- DATA_PROJECTS/deliveries_car_manufacturers/scripts/vw_extract_and_append_data.py
+++ DATA_PROJECTS/deliveries_car_manufacturers/scripts/vw_extract_and_append_data.py
@@ -1,42 +1,6 @@
 import os
-import pdfplumber
 import pandas as pd
 from vw_extract_regions_deliveries_table import extract_first_table
-
-
-# def extract_first_table(pdf_path):
-#     """Extracts the region and latest month columns from the first table of a PDF."""
-#     with pdfplumber.open(pdf_path) as pdf:
-#         first_page = pdf.pages[0]
-#         tables = first_page.extract_tables()
-
-#         if not tables:
-#             print(f"No tables found in {pdf_path}")
-#             return None
-
-#         df = pd.DataFrame(tables[0])
-
-#         if df.shape[1] < 2:
-#             print(f"Unexpected table structure in {pdf_path}")
-#             return None
-
-#         region_col = df[0].str.strip()
-#         value_col = df[1].str.replace(",", "").astype(float, errors="ignore")
-
-#         latest_month_data = pd.DataFrame(
-#             {
-#                 "Region": region_col,
-#                 os.path.basename(pdf_path).replace(".pdf", ""): value_col,
-#             }
-#         )
-
-#         for index in range(len(latest_month_data)):
-#             if latest_month_data.iloc[index, 0] == "":
-#                 latest_month_data.iloc[index, 0] = "Worldwide"
-
-#         latest_month_data.dropna(inplace=True)
-
-#         return latest_month_data
 
 
 def build_dataset_from_pdfs(pdf_dir, output_csv="vw_deliveries.csv"):
@@ -55,6 +19,10 @@ def build_dataset_from_pdfs(pdf_dir, output_csv="vw_deliveries.csv"):
 
             for _, row in pdf_df.iterrows():
                 region = row["Region"]
+
+                if "China" in region:
+                    region = "China"
+
                 latest_month_value = row[filename.replace(".pdf", "")]
 
                 if region in data_dict:
@@ -65,7 +33,7 @@ def build_dataset_from_pdfs(pdf_dir, output_csv="vw_deliveries.csv"):
                     }
 
     all_data = pd.DataFrame.from_dict(data_dict, orient="index").reset_index()
-    all_data.columns = ["Region"] + list(all_data.columns[1:])  # Rename columns
+    all_data.columns = ["Region"] + list(all_data.columns[1:])
 
     all_data.to_csv(output_csv, index=False)
     print(f"Data saved to {output_csv}")
